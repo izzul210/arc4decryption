a3ad9d942e5106824164c99dcd2c449fd17bffae
Izzul Syahmi <68768489+izzul210@users.noreply.github.com>
2020-07-25 18:15:11 -0700

    Update cracking_arc4.sv

--- cracking_arc4.sv
+++ cracking_arc4.sv
@@ -1,4 +1,4 @@
-module task4(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
+module cracking_arc4(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [6:0] HEX0, output logic [6:0] HEX1, output logic [6:0] HEX2,
              output logic [6:0] HEX3, output logic [6:0] HEX4, output logic [6:0] HEX5,
              output logic [9:0] LEDR);
